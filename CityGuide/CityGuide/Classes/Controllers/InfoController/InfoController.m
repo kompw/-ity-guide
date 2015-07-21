9ad4ac8b286d170c5f401d62a9d5b090b6803233
Tatas Sorokin <Taras_Sorokin@epam.com>
2015-07-21 23:15:27 +0300

    new dez 07

--- CityGuide/CityGuide/Classes/Controllers/InfoController/InfoController.m
+++ CityGuide/CityGuide/Classes/Controllers/InfoController/InfoController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.title = @"О приложении";
+    self.navigationItem.rightBarButtonItem = [AppManager plusButton:self andSelector:@selector(openSendController)];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +25,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)openSendController{
+    [self.navigationController pushViewController:[SendMessageViewController new] animated:YES];
 }
-*/
 
 @end
